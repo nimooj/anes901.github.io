73734e1f4b679e6e868ebefc606e6d8d3db629b2
anes901 <anes901@gmail.com>
2015-07-07 11:02:34 +0900

    2015.7.7.ver1

--- script/test.js
+++ script/test.js
@@ -1,3 +1,10 @@
+var menuList = [{"title" : "HOME", "url" : "/index.html", "status" : "true"},
+				{"title" : "ABOUT", "url" : "/about.html", "status" : "true"},
+				{"title" : "PHOTO", "url" : "/photo.html", "status" : "false"},
+				{"title" : "CONTACT", "url" : "/contact.html", "status" : "true"},
+				{"title" : "QUICK MENU", "url" : "", "status" : "true"}
+				];
+
 function changePhoto() {
 	var image = document.getElementById('giraffe_front');
 	if(image.src.match("front")) {
@@ -8,26 +15,8 @@ function changePhoto() {
 	}
 }
 
-function move_page(state) {
-	console.log("move_page()");
-
-	if(state == "false")
-	{
-		alert("아직 준비중인 페이지입니다.");
-	}
-}
-
-var menuList = [{"title" : "HOME", "url" : "/index.html", "status" : "true"},
-				{"title" : "ABOUT", "url" : "/about.html", "status" : "true"},
-				{"title" : "PHOTO", "url" : "/photo.html", "status" : "false"},
-				{"title" : "CONTACT", "url" : "/contact.html", "status" : "true"},
-				{"title" : "QUICK MENU", "url" : "", "status" : "true"}
-				];
-
 function create_menu(){
-
 	for (var i = 0; i < menuList.length - 1; i++) {
-		console.log("create_menu\n");
 		var className = document.getElementById("select");
 
 		var ElementType = document.createElement('a');
@@ -35,20 +24,19 @@ function create_menu(){
 		var title = document.createTextNode(menuList[i].title);
 
 		ElementType.appendChild(title);
-		ElementType.setAttribute('href', menuList[i].url);
 
-ElementType.addEventListener('click', move_page(menuList[i].status));
-/*
-		ElementType.setAttribute('onclick', move_page(menuList[i].status));
-		ElementType.onclick = function() { move_page(menuList[i].status); };
-		*/
+		if(menuList[i].status == "true") {
+			ElementType.setAttribute('href', menuList[i].url);
+		}
+		else {
+			ElementType.addEventListener('click', function(){ alert("아직 준비 중인 페이지입니다."); } );
+		}
 
 		className.appendChild(ElementType);
 	};
 }
 
 function quick_menu() {
-	console.log("quick_menu");
 	var className = document.getElementById("quick_menu");
 
 	var element = document.createElement('a');
@@ -65,7 +53,13 @@ function quick_menu() {
 		var list_title = document.createTextNode(menuList[i].title);
 
 		list.appendChild(list_title);
-		list.setAttribute('href', menuList[i].url);
+
+		if(menuList[i].status == "true") {
+			list.setAttribute('href', menuList[i].url);
+		}
+		else {
+			list.addEventListener('click', function(){ alert("아직 준비 중인 페이지입니다.");});
+		}
 
 		createList.appendChild(list);
 	};
